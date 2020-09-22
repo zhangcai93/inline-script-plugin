d2af59ac3703818d3cc4ef82ea5c0731a1f9ebf0
zhangcai <zhangcai@didichuxing.com>
2020-09-22 16:20:51 +0800

    share

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# inline-script-plugin
+## inline-script-plugin
+
+加强 `html-webpack-plugin `插件
+
+将给定路径的script插入到所有js脚本的最前面, 或直接插入到head标签中
+
+提供以inline的形式插入到html中
+
+使用前确保有安装 `html-webpack-plugin `
+
+### 安装
+
+```javascript
+npm i inline-script-plugin -D
+```
+
+### 使用
+
+将`inline-script-plugin`添加到`webpack`插件配置当中：
+
+```javascript
+// vue.config.js为例
+const InlineScriptPlugin = require('inline-script-plugin')
+
+module.exports = {
+  xxx,
+  chainWebpack: config => {
+    config
+      .plugin('InlineScriptPlugin')
+      .after('html')
+      .use(InlineScriptPlugin, [
+        {
+          name: 'abc', // yourName
+          path: './a/b.js', // yourPath
+          inhead: true,
+          inline: true
+        }
+      ])
+      .end()
+  }
+}
+```
+
+有关 options 配置如下：
+
+|字段名|参数类型|默认值|是否必填|说明|
+|--|--|--|--|--|
+| name | String | - | 是 | 文件名 | 
+| path | String | - | 是 | 文件路径 |
+| inline | Boolean | false | 否 | 是否以inline形式插入脚本 |
+| inhead | Boolean | false | 否 | 是否将脚本插入到head标签里 |
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const {
   getHeadAndBodyChunks,
   getInlineChunks,
   addEntryPoint,
-  modifySplitChunks
+  modifySplitChunks,
+  sortJsChunks
 } = require('./lib/utils.js')
+const { INHEAD_SUFFIX } = require('./lib/constants.js')
 
 class InlineScriptPlugin {
   constructor(options = {}) {
@@ -12,7 +14,7 @@ class InlineScriptPlugin {
       const message = 'either reuqired options name or path did not pass'
       throw new Error(message)
     }
-    this.name = inhead ? `${name}_inhead` : name
+    this.name = inhead ? `${name}${INHEAD_SUFFIX}` : name
     this.inline = inline
     this.entryPoint = {
       [this.name]: Array.isArray(path) ? path : [path]
@@ -26,19 +28,28 @@ class InlineScriptPlugin {
     const finalEntry = addEntryPoint(originEntry, this.entryPoint)
     compiler.options.entry = finalEntry
 
-
+    // entry公共模块拆分
     const splitChunks = compiler.options.optimization.splitChunks
     compiler.options.optimization.splitChunks = modifySplitChunks(splitChunks, this.name)
 
-    // HtmlWebpackPlugin version 4.0.0-beta.5
+    // HtmlWebpackPlugin version 4.x
     const HtmlWebpackPlugin = require('html-webpack-plugin')
     if (HtmlWebpackPlugin.getHooks) {
       compiler.hooks.compilation.tap(selfName, compilation => {
+        HtmlWebpackPlugin.getHooks(compilation).onBeforeHtmlGeneration.tapAsync(
+          selfName,
+          (data, callback) => {
+            const { assets } = data
+            // 调整js顺序 传入的entry插入在最上部
+            assets.js = sortJsChunks(assets.js, this.name)
+            callback(null, data)
+          }
+        )
         HtmlWebpackPlugin.getHooks(compilation).alterAssetTagGroups.tapAsync(
           selfName,
           (data, callback) => {
             const tags = [...data.bodyTags, ...data.headTags]
-            const chunks = getHeadAndBodyChunks(tags, this.name)
+            const chunks = getHeadAndBodyChunks(tags)
 
             const { headChunks, bodyChunks } = getInlineChunks(compilation, chunks, this.name, this.inline)
 
@@ -52,10 +63,16 @@ class InlineScriptPlugin {
     } else {
       // HtmlWebpackPlugin version 3.2.x
       compiler.plugin('compilation', compilation => {
+        compilation.plugin('html-webpack-plugin-before-html-generation', data => {
+          const { assets } = data
+          // 调整js顺序 传入的name插入在最上部
+          assets.js = sortJsChunks(assets.js, this.name)
+        })
+        
         compilation.plugin('html-webpack-plugin-alter-asset-tags', data => {
           const tags = [...data.body, ...data.head]
           // _inhead后缀script添加到头部
-          const chunks = getHeadAndBodyChunks(tags, this.name)
+          const chunks = getHeadAndBodyChunks(tags)
           // 转为inline形式
           const { headChunks, bodyChunks } = getInlineChunks(compilation, chunks, this.name, this.inline)
       
